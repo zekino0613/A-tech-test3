d77fde7ee624336cef2987e91343ea154042d2dc
zekino <zekino.0613@gmail.com>
2025-04-22 09:06:58 +0900

    mainvisual-SP

--- front-page.php
+++ front-page.php
@@ -5,24 +5,25 @@
   <!-- ---------------------------------------------------------------------->
 
 	<main>
+		<section id="mainvisual">
+			<div class="mainvisual__fv">
+				
+			</div>
+		</section>
 		
-
-
-
-
-
-	<section id="mainvisual">mainvisual</section>
-	<section id="property">property</section>
-	<section id="service">service</section>
-	<section id="about">about</section>
-	<section id="recruit">recruit</section>
-	<section id="news">news</section>
+		
+		
+		<section id="property">property</section>
+		<section id="service">service</section>
+		<section id="about">about</section>
+		<section id="recruit">recruit</section>
+		<section id="news">news</section>
   <!-- ---footer読み込み ----------------------------------------------->
   <?php
     get_template_part('template-parts/footer'); // footer.php をインクルード
   ?>
   <!-- ---------------------------------------------------------------------->
-</main>
+	</main>
 
 
 </body>
